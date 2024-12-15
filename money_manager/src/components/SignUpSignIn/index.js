9f6d7a94322bda460e0cc9789ff75863b8ae9f8d
Saikrishna216 <krishnaksai216@gmail.com>
2024-12-15 19:21:16 +0530

    Created React Firebase hooks and dashboards and implemented the ant design for dashboard

--- money_manager/src/components/SignUpSignIn/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./styles.css";
-function SignUpSignInComponent(){
-    return <div className="signup-wrapper">Signup Page</div>
-}
-
-export default SignUpSignInComponent;
