16be3cf2d610c3df7dc5870247e813107bad572d
Saikrishna216 <krishnaksai216@gmail.com>
2024-12-15 15:00:20 +0530

    Completed the firebase setup and completed the react router and landing page and authentication with the firebase setup.

--- money_manager/src/App.js
+++ money_manager/src/App.js
@@ -1,10 +1,16 @@
-
-import './App.css';
-import Header from './components/Header';
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import SignUpSignIn from "./components/Signup";
 
 function App() {
   return (
-    <div className="App"><Header/></div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<SignUpSignIn />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ money_manager/src/components/SignUpSignIn/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./styles.css";
+function SignUpSignInComponent(){
+    return <div className="signup-wrapper">Signup Page</div>
+}
+
+export default SignUpSignInComponent;
